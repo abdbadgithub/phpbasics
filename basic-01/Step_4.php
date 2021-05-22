c15d67c9c07b60b299a5b87647a28470bfcea788
abdallah badra <abdallahbadra1993@gmail.com>
2021-05-22 12:05:06 +0300

    Step4

---
+++ basic-01/Step_4.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Hello, I'm Gaby,";
+$position = "I'm a tech consultant please check my github link ";
+$github_url = "https://github.com/gk-git";
+$output = 'Hello, I\'m Gaby, I\'m a tech consultant please check my github link https://github.com/gk-git';
+// Output
+echo $name.' '.$position.' '.$github_url;
+echo "<br>";
+echo $output;
+?>
