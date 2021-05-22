7785b7f5429519ac7fa723ea493c739eee74468e
abdallah badra <abdallahbadra1993@gmail.com>
2021-05-22 12:22:30 +0300

    Step5

---
+++ basic-01/Step_5.php
@@ -0,0 +1,14 @@
+<?php
+$course_name = "Rust, the language of no bugs";
+$enrolled_students = 20;
+$price = 30.0;
+$on_discount = false;
+echo $course_name;
+echo "<br>";
+echo $enrolled_students;
+echo "<br>";
+echo $price;
+echo "<br>";
+echo  $on_discount = $on_discount == true ? 'Yes' : 'No';
+echo "<br>";
+?>
