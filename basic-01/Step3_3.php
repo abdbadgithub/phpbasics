18f831823cf1d3ca6188359e386dca4fcb80e040
abdallah badra <abdallahbadra1993@gmail.com>
2021-05-22 11:56:07 +0300

    Step3

---
+++ basic-01/Step3_3.php
@@ -0,0 +1,12 @@
+<?php
+$name = 'abdallah';
+$email = 'abdallahbadra1993@gmail.com';
+$output = $name .' '.$email;
+// Output
+echo "my name and email are ".$output;
+echo'<br>';
+$height=190;
+$width=20;
+$area = $height * $width;
+echo "area equal to ".$area;
+?>
