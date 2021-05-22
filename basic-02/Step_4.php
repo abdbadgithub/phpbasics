c6924f9846703b244e1ad6123cd58d6a8ae9da44
abdallah badra <abdallahbadra1993@gmail.com>
2021-05-22 13:55:50 +0300

    Step4-basic2

---
+++ basic-02/Step_4.php
@@ -0,0 +1,21 @@
+<?php  
+// 0, 1, 153, 371, 407, 471, etc are Armstrong numbers.
+$num=407;  
+$total=0;  
+$x=$num;  
+while($x!=0)  
+{  
+$rem=$x%10;  
+$total=$total+$rem*$rem*$rem;  
+$x=$x/10;  
+}  
+if($num==$total)  
+{  
+echo "Yes it is an Armstrong number";  
+}  
+else  
+{  
+echo "No it is not an armstrong number";  
+}  
+
+?> 
