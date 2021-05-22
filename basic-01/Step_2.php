b35771c623f12e40f71767c6f86a843a014d78b2
abdallah badra <abdallahbadra1993@gmail.com>
2021-05-22 11:48:13 +0300

    error

---
+++ basic-01/Step_2.php
@@ -0,0 +1,5 @@
+<?php
+echo sadsadsadsadasdsad
+//Warning: Use of undefined constant sadsadsadsadasdsad - assumed 'sadsadsadsadasdsad' (this will throw an Error in a future version of PHP) in /var/www/html/basicphp/basic-01/Step_2.php on line 2
+
+?>
